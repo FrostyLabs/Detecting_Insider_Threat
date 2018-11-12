039dc7f2a621f99d05d809ee742af01f112e7a47
Oliver Thornewill <40210534+othornew@users.noreply.github.com>
2018-11-12 12:19:52 +0000

    Update Scenario 1/json_script.py

--- Scenario 1/json_script.py
+++ Scenario 1/json_script.py
@@ -8,6 +8,8 @@
 # Import packages
 import json
 import sys
+import os
+import datetime
 
 
 def json_format(text):
@@ -18,7 +20,7 @@ def json_format(text):
 
     # If we take seperate items from the list, we can parse it into JSON
     json.loads(json_data[0])
-    print("loaded json_data", '\n\n\n\n')
+    print("loaded json_data", 'Preparing for analysis \n')
 
     # The JSON data structure is practically the same as the Python Dictionary, so if we can load them step by step
     # then we can comebine them into a single one using a dictionary
@@ -38,23 +40,52 @@ def json_format(text):
 
 
 def login_attempts(text):
-    """login attempts"""
-    json_file = open("formatted_cowrie.json", "r", encoding="utf-8")
-    data = json.load(json_file)
-    json_file.close()
-    # print (json_data)
-    for i in data.values():
-        if 'login' in i['eventid']:
-            if 'success' in i['eventid']:
-                print("***Successful Credentials:", i["username"], '/', i["password"])
-            else:
-                print("Failed attempts", i["username"], '/', i["password"])
+	"""login attempts"""
+	json_file = open("formatted_cowrie.json", "r", encoding="utf-8")
+	data = json.load(json_file)
+	json_file.close()
+	# print (json_data)
+	for i in data.values():
+		if 'login' in i['eventid']:
+			if 'success' in i['eventid']:
+				print("** Successful Credentials:", i["username"], '/', i["password"], '\n',
+				"      IP Address:", i['src_ip'], '\n',
+				"      Time:", datetime.datetime.strptime(i['timestamp'][:-8], '%Y-%m-%dT%H:%M:%S'))
+				print()
+			else:
+				print("!! Failed attempts", i["username"], '/', i["password"], '\n',
+				"      IP Address:", i['src_ip'], '\n',
+				"      Time:", datetime.datetime.strptime(i['timestamp'][:-8], '%Y-%m-%dT%H:%M:%S'), '\n')
+
+def commands(text):
+	"""IP addresses seen"""
+	json_file = open("formatted_cowrie.json", "r", encoding="utf-8")
+	data = json.load(json_file)
+	json_file.close()
+	for i in data.values():
+		if 'input' in i['eventid']:
+			print("Command used:",i['input'], "from",  i['src_ip'], '\n',
+			"   time:", datetime.datetime.strptime(i['timestamp'][:-8], '%Y-%m-%dT%H:%M:%S'), '\n')
+
+def session_activity(text):
+	"""Find session activity"""
+	json_file = open("formatted_cowrie.json", "r", encoding="utf-8")
+	data = json.load(json_file)
+	json_file.close()
+	for i in data.values():
+		if i['session'] == i['session']: # Of course this will always result in True; need to figure out a solution
+			print (i['session'])
 
 
 def main():
-    print("Analyzing file:", sys.argv[1])
-    json_format(sys.argv[1])
+	if os.path.isfile('formatted_cowrie.json'):
+		sys.argv.append('formatted_cowrie.json')
+
+	print("Analyzing file:", sys.argv[1])
+	json_format(sys.argv[1])
 	login_attempts(sys.argv[1])
+	#commands(sys.argv[1])
+	session_activity(sys.argv[1])
 
 
 if __name__ == '__main__':
