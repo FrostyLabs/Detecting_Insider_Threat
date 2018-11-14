42c1b955a10502a37af20ffdb82062dfecd0475a
Oliver Thornewill <40210534+othornew@users.noreply.github.com>
2018-11-14 18:00:21 +0000

    Update Scenario 1/small.py
    Added documentation / the point of this script.

--- Scenario 1/small.py
+++ Scenario 1/small.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3.6
+
+"""
+This python script is only using a small set of data.
+The point of this script is to simply be a proof of concept file, allowing me to try different methods
+
+@author: othornew
+"""
+
 import json
 
 
@@ -44,31 +53,24 @@ data ={
 		}
 
 
-import collections
-
-grouped = collections.defaultdict(list)
-
-
-for d in data.values():
-	grouped[d['session']].append(d)
-
-#print(grouped)
-
-#{'1eeaaf20904f': [{'eventid': 'login.success', 'username': 'sam', 'password': 'p4ssw0rd', 'message': 'login attempt [sam/p4ssw0rd] succeeded', 'sensor': 'ubuntu', 'timestamp': '2018-11-11T13:50:53.064183Z', 'src_ip': '192.168.217.131', 'session': '1eeaaf20904f'}, {'eventid': 'command.input', 'input': 'ls -la', 'message': 'CMD: ls -la', 'sensor': 'ubuntu', 'timestamp': '2018-11-11T13:51:01.293303Z', 'src_ip': '192.168.217.131', 'session': '1eeaaf20904f'}],
-#'6fac825b1545': [{'eventid': 'login.failed', 'username': 'george', 'password': 'george', 'message': 'login attempt [george/george] failed', 'sensor': 'ubuntu', 'timestamp': '2018-11-11T14:01:24.658923Z', 'src_ip': '192.168.217.131', 'session': '6fac825b1545'}, {'eventid': 'login.failed', 'username': 'george', 'password': 'hello', 'message': 'login attempt [george/hello] failed', 'sensor': 'ubuntu', 'timestamp': '2018-11-11T14:01:32.376158Z', 'src_ip': '192.168.217.131', 'session': '6fac825b1545'}]}
-
-
-for i in grouped.values():
-	print(i[1])
-
-	# "problem" with this is that it prints the first value of the grouped. Need to figure out how I can use this for analysis
+grouped = {}
+for log in data.values():
+	session_id = log['session']
+	if session_id not in grouped:
+		grouped[session_id] = []
+	grouped[session_id].append(log)
+print (grouped,'\n')
 
 
 
-# for i in data.values():
-# 	if 'login' in i['eventid']:
-# 		if 'success' in i['eventid']:
-# 			#print(i)
-# 			print("Successful login:",i["username"],'/', i["password"])
-# 		else:
-# 			print("Failed attempts",i["username"],'/', i["password"])
+from itertools import groupby
+from operator import itemgetter
+
+groupd = {
+    group[0]: [log for log in group[1]]
+    for group in groupby(
+        [v for v in data.values()],
+        key=itemgetter("session")
+    )
+}
+print(groupd, '\n')
