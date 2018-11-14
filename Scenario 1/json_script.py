151773f9f7592b4f28a8567a1f5f64c72229c44d
Oliver Thornewill <40210534+othornew@users.noreply.github.com>
2018-11-14 17:59:56 +0000

    Added log grouping
    Added function ("group_session_id") grouping the logs based on the unique session key.
    
    Other improvements cleaning up use of variables

--- Scenario 1/json_script.py
+++ Scenario 1/json_script.py
@@ -20,7 +20,7 @@ def json_format(text):
 
     # If we take seperate items from the list, we can parse it into JSON
     json.loads(json_data[0])
-    print("loaded json_data", 'Preparing for analysis \n')
+    print(sys.argv[1], 'successfully loaded! \nPreparing for analysis \n')
 
     # The JSON data structure is practically the same as the Python Dictionary, so if we can load them step by step
     # then we can comebine them into a single one using a dictionary
@@ -38,54 +38,91 @@ def json_format(text):
     with open('formatted_cowrie.json', 'w') as fp:
         json.dump(json_dict, fp)
 
+    print('Successfully formatted the file:', sys.argv[1], '\nSaved under \'formatted_cowrie.json\'')
+    print('\nPlease re-run this script ')
 
-def login_attempts(text):
+
+def login_attempts(formatted_json):
 	"""login attempts"""
-	json_file = open("formatted_cowrie.json", "r", encoding="utf-8")
+	json_file = open(formatted_json, "r", encoding="utf-8")
 	data = json.load(json_file)
 	json_file.close()
-	# print (json_data)
+
+	successes = 0
+	failures = 0
+
 	for i in data.values():
 		if 'login' in i['eventid']:
 			if 'success' in i['eventid']:
+				successes = successes + 1
 				print("** Successful Credentials:", i["username"], '/', i["password"], '\n',
-				"      IP Address:", i['src_ip'], '\n',
-				"      Time:", datetime.datetime.strptime(i['timestamp'][:-8], '%Y-%m-%dT%H:%M:%S'))
+				"   IP Address:", i['src_ip'], '\n',
+				"   Time:", datetime.datetime.strptime(i['timestamp'][:-8], '%Y-%m-%dT%H:%M:%S'))
 				print()
-			else:
+			elif 'failed' in i['eventid']:
+				failures = failures + 1
 				print("!! Failed attempts", i["username"], '/', i["password"], '\n',
-				"      IP Address:", i['src_ip'], '\n',
-				"      Time:", datetime.datetime.strptime(i['timestamp'][:-8], '%Y-%m-%dT%H:%M:%S'), '\n')
+				"   IP Address:", i['src_ip'], '\n',
+				"   Time:", datetime.datetime.strptime(i['timestamp'][:-8], '%Y-%m-%dT%H:%M:%S'), '\n')
 
-def commands(text):
+	print("--> Successful Logins", successes)
+	print("--> Failed Logins", failures)
+
+def commands(formatted_json):
 	"""IP addresses seen"""
-	json_file = open("formatted_cowrie.json", "r", encoding="utf-8")
+	json_file = open(formatted_json, "r", encoding="utf-8")
 	data = json.load(json_file)
 	json_file.close()
+
+	commands = 0
+
 	for i in data.values():
 		if 'input' in i['eventid']:
-			print("Command used:",i['input'], "from",  i['src_ip'], '\n',
+			commands = commands + 1
+			print("Command used: \n ",i['input'], "from",  i['src_ip'], '\n',
 			"   time:", datetime.datetime.strptime(i['timestamp'][:-8], '%Y-%m-%dT%H:%M:%S'), '\n')
 
-def session_activity(text):
-	"""Find session activity"""
-	json_file = open("formatted_cowrie.json", "r", encoding="utf-8")
+	print("Number of commands:", commands)
+
+
+def group_session_id(formatted_json):
+	"""This function groups the json data by session value"""
+	print('Session_ID Grouping \n')
+
+	json_file = open(formatted_json, "r", encoding="utf-8")
 	data = json.load(json_file)
 	json_file.close()
-	for i in data.values():
-		if i['session'] == i['session']: # Of course this will always result in True; need to figure out a solution
-			print (i['session'])
+
+	grouped = {}
+	for log in data.values():
+		session_id = log['session']
+		if session_id not in grouped:
+			grouped[session_id] = []
+		grouped[session_id].append(log)
 
 
-def main():
+# Not yet complete
+
+# def session_activity(text):
+# 	"""Find session activity"""
+# 	print('\n Session Activity \n')
+
+
+def main(): #run by ./json_script.py cowrie.json
+	"""Main function"""
+	print("--> Analyzing file:", sys.argv[1], '\n')
+	print('--> Checking if relevant files exist')
 	if os.path.isfile('formatted_cowrie.json'):
 		sys.argv.append('formatted_cowrie.json')
-
-	print("Analyzing file:", sys.argv[1])
-	json_format(sys.argv[1])
-	login_attempts(sys.argv[1])
-	#commands(sys.argv[1])
-	session_activity(sys.argv[1])
+		login_attempts(sys.argv[2])
+		print('\n\n')
+		commands(sys.argv[2])
+		print('\n')
+		group_session_id(sys.argv[2])
+		# Not yet complete
+		#session_activity(sys.argv[2])
+	else:
+		json_format(sys.argv[1])
 
 
 if __name__ == '__main__':
