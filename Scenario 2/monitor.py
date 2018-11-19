bdbc0b1eb87373aa8a861a6effd68c476a22534b
Oliver Thornewill <40210534+othornew@users.noreply.github.com>
2018-11-19 12:14:45 +0000

    Create monitor.py

---
+++ Scenario 2/monitor.py
@@ -0,0 +1,53 @@
+"""
+@author Brumo Rocha
+	#http://brunorocha.org/python/watching-a-directory-for-file-changes-with-python.html
+	#https://github.com/rochacbruno
+
+"""
+
+import sys
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+class MyHandler(PatternMatchingEventHandler):
+	patterns = ["*.xml", "*.lxml", "*.txt"]
+
+	def process(self, event):
+		"""
+		event.event_type
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        event.src_path
+            path/to/observed/file
+        """
+		# The file will be processed there
+		print event.src_path, event.event_type
+	def on_modified(self, event):
+		#self.process(event)
+		#print('Modified File', event)
+		print('Modified File')
+
+	def on_created(self, event):
+		self.process(event)
+
+	def on_deleted(self, event):
+		self.process(event)
+
+	def on_moved(self, event):
+		self.process(event)
+
+if __name__ == '__main__':
+	args = sys.argv[1:]
+	observer = Observer()
+	observer.schedule(MyHandler(), path=args[0] if args else '.')
+	observer.start()
+
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		observer.stop()
+
+	observer.join()
