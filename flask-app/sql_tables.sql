d6e43d80f921fde7245967bbc0ec0bf608894edb
Oliver Thornewill <40210534+othornew@users.noreply.github.com>
2019-02-08 09:45:45 +0000

    Update sql_tables.sql

--- flask-app/sql_tables.sql
+++ flask-app/sql_tables.sql
@@ -7,21 +7,21 @@ CREATE TABLE stats(
   total int(5)
 );
 
-DROP TABLE users; 
+DROP TABLE users;
 CREATE TABLE users(
-	id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-	name varchar(100), 
-	email varchar(100), 
-	username varchar(30), 
-	password varchar(100), 
+	id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	name varchar(100),
+	email varchar(100),
+	username varchar(30),
+	password varchar(100),
 	register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-DROP TABLE articles; 
+DROP TABLE articles;
 CREATE TABLE articles(
-	id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-	title varchar(255), 
-	author varchar(100), 
-	body TEXT, 
+	id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	title varchar(255),
+	author varchar(100),
+	body TEXT,
 	create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
